965a95e416bde4f853f22d72cdc2f60230766223
Luca Bolognese <lucabol@microsoft.com>
2019-09-16 12:41:43 +0200

    Making it first class

--- Program.cs
+++ Program.cs
@@ -8,61 +8,25 @@ class Program
 {
     public static async Task Main()
     {
-        //NullableDemo.NullableRefTypes();
-        await AsyncStreamDemo.AsyncStream();
+        await AsyncStream();
     }
 
-    class NullableDemo
+    internal static async IAsyncEnumerable<int> GenerateSequence()
     {
-        internal class Person
+        for (int i = 0; i < 20; i++)
         {
-            public string FirstName { get; set; }
-            public string? MiddleName { get; set; }
-            public string LastName { get; set; }
-
-            public Person(string first, string last) =>
-                (FirstName, LastName) = (first, last);
-
-            public Person(string first, string middle, string last) =>
-                (FirstName, MiddleName, LastName) = (first, middle, last);
-
-            public override string ToString() => $"{FirstName} {MiddleName} {LastName}";
-        }
-
-        private static int GetLengthOfMiddleName(Person p)
-        {
-            string? middleName = p.MiddleName;
-            return middleName?.Length ?? 0;
-        }
-
-
-        public static void NullableRefTypes()
-        {
-            Person miguel = new Person("Luca", "Bolognese");
-            var length = GetLengthOfMiddleName(miguel);
-            Console.WriteLine(length);
+            // every 3 elements, wait 2 seconds:
+            if (i % 3 == 0)
+                await Task.Delay(2000);
+            yield return i;
         }
     }
 
-    class AsyncStreamDemo
+    public async static Task AsyncStream()
     {
-        internal static async IAsyncEnumerable<int> GenerateSequence()
+        await foreach (var number in GenerateSequence())
         {
-            for (int i = 0; i < 20; i++)
-            {
-                // every 3 elements, wait 2 seconds:
-                if (i % 3 == 0)
-                    await Task.Delay(2000);
-                yield return i;
-            }
-        }
-
-        public async static Task AsyncStream()
-        {
-            await foreach (var number in GenerateSequence())
-            {
-                Console.WriteLine($"The time is {DateTime.Now:hh:mm:ss}. Retrieved {number}");
-            }
+            Console.WriteLine($"The time is {DateTime.Now:hh:mm:ss}. Retrieved {number}");
         }
     }
 }
