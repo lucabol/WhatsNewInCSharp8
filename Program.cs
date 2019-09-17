90962c45b273a3a43e5e2b291c546986b0286b49
Luca Bolognese <lucabol@microsoft.com>
2019-09-17 12:05:43 +0200

    First commit.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,27 @@
-﻿public class Program
+﻿using System;
+using System.Collections.Generic;
+
+public struct Point
+{
+    public double X { get; set; }
+    public double Y { get; set; }
+    public double Distance => Math.Sqrt(X * X + Y * Y);
+
+    public override string ToString() =>
+        $"({X}, {Y}) is {Distance} from the origin";
+
+    public void Translate(int xOffset, int yOffset)
+    {
+        X += xOffset;
+        Y += yOffset;
+    }
+}
+
+class Program
 {
     static void Main()
     {
-        System.Diagnostics.Process.Start("pwsh", @"c:\dev\WhatsNewInCSharp8\Reset.ps1");
+        var p = new Point() { X = 30, Y = 40 };
+        Console.WriteLine(p);
     }
 }
